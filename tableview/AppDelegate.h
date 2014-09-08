9a773ca17a714e735095ea348163bff6bdc08402
mac <mac@mactekiMacBook-Pro.local>
2014-09-08 12:44:48 +0800

    Initial Commit

---
+++ tableview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tableview
+//
+//  Created by mac on 14-9-8.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tableview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tableview
+//
+//  Created by mac on 14-9-8.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
